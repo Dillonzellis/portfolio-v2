9fbfd40ac92a0e1fa4d248bf2f0bb839968027c9
dillonzellis <dillonzellis@gmail.com>
2023-09-15 15:42:11 -0400

    add tw prettier sorting

--- components/sections/Intro.tsx
+++ components/sections/Intro.tsx
@@ -4,21 +4,21 @@ import { Button } from "@/components/ui/ButtonLink";
 
 export const Intro = () => {
   return (
-    <section className="max-w-prose mx-auto mt-12">
-      <div className="flex justify-center items-center pb-8">
+    <section className="mx-auto mt-12 max-w-prose">
+      <div className="flex items-center justify-center pb-8">
         <Image
           src={donkeyhmm}
           alt="danky kang"
           width={100}
           height={100}
-          className="w-[100px] h-[100px] object-cover flex items-center justify-center rounded-full shadow-xl border-2 border-white"
+          className="flex h-[100px] w-[100px] items-center justify-center rounded-full border-2 border-white object-cover shadow-xl"
         />
       </div>
-      <h1 className="text-2xl text-center pb-4">
+      <h1 className="pb-4 text-center text-2xl">
         Hello, I'm Dillon. I'm a full-stack developer with 8 years of
         experience. I enjoy building sites & apps. My focus is React (Next.js).
       </h1>
-      <div className="flex flex-col sm:flex-row items-center justify-center gap-2">
+      <div className="flex flex-col items-center justify-center gap-2 sm:flex-row">
         <Button intent="dark">Contact Me</Button>
         <Button intent="light">Download Resume</Button>
       </div>
