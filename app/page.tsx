8421e524aabf4d21bf9defdcbf75192099e35861
dillonzellis <dillonzellis@gmail.com>
2023-09-15 15:38:59 -0400

    add section divider

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { SectionDivider } from "@/components/SectionDivider";
 import { Intro } from "@/components/sections/Intro";
 
 export default function Home() {
   return (
     <main>
       <Intro />
+      <SectionDivider />
     </main>
   );
 }
