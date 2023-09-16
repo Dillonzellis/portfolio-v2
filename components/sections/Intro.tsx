91af0b2b219ef1a8ebedfb45049a32e6c73d86a2
dillonzellis <dillonzellis@gmail.com>
2023-09-16 12:51:57 -0400

    intro print width

--- components/sections/Intro.tsx
+++ components/sections/Intro.tsx
@@ -16,7 +16,8 @@ export const Intro = () => {
       </div>
       <h1 className="pb-4 text-center text-2xl">
         Hello, I'm Dillon. I'm a full-stack developer with 8 years of
-        experience. I enjoy building sites & apps. My focus is React (Next.js).
+        experience. I enjoy building sites & apps. My focus is React
+        (Next.js).
       </h1>
       <div className="flex flex-col items-center justify-center gap-2 sm:flex-row">
         <Button intent="dark">Contact Me</Button>
